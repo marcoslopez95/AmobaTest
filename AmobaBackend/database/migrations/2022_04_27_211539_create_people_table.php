d8e64ef047df9e2acb8c3f2c4e1d90886310134b
Marcos L <lopezm@zippyttech.com>
2022-04-27 17:56:28 -0400

    primer commit

---
+++ AmobaBackend/database/migrations/2022_04_27_211539_create_people_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePeopleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('persons', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('document');
+            $table->string('ima_profile');
+            $table->enum('type_person',[0,1])->default(1);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('people');
+    }
+}
